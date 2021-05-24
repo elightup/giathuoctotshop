74696ac49c4a581eebc8d528cd1afab168478498
longvt93 <vutuanlongdhcn@gmail.com>
2021-05-24 17:26:05 +0700

    Làm lại trang cart giống thuocsi

--- templates/cart.php
+++ templates/cart.php
@@ -4,6 +4,7 @@
 <script type="text/html" id="tmpl-cart">
 	<#
 	let total = 0;
+	let total_quantity = 0;
 	let id = 0;
 	let giam_gia = 0;
 	let cart_subtotal = 0;
@@ -13,67 +14,80 @@
 		<#
 	} else {
 		#>
-		<table class="cart cart-checkout table table-bordered">
-			<thead class="thead-dark">
-			    <tr>
-			      <th scope="col">#</th>
-			      <th scope="col">Tên sản phẩm</th>
-			      <th scope="col">Số lượng</th>
-			      <th scope="col">Giá</th>
-			      <th scope="col">Tổng tiền</th>
-			      <th scope="col">Thao tác</th>
-			    </tr>
-		  	</thead>
-		  	<tbody>
-			<#
-			data.products.forEach( product => {
-				var subtotal = product.price * product.quantity;
-				total += subtotal;
-				id += 1;
-				#>
-				<tr>
-					<td class="cart__stt">{{ id }}</td>
-
-					<td class="cart__title">
-						<a href="{{ product.link }}"><img src="{{product.url}}" alt="{{product.title}}" />{{ product.title }}</a>
-					</td>
-					<td class="cart__quantity"><input type="number" value="{{ product.quantity }}" min="1" data-product_id="{{ product.id }}" style="width: 70px;float: initial;margin: auto;text-align: center;"></td>
-					<td class="cart__price"><div class="price__coin">{{ eFormatNumber(0, 3, '.', ',', parseFloat( product.price )) }} <?= $symbol; ?></div></td>
-					<td class="cart__subtotal"><span class="cart__subtotal__number">{{ eFormatNumber(0, 3, '.', ',', parseFloat( subtotal )) }}</span> <?= $symbol; ?></td>
-					<td class="cart__remove-product"> <button class="cart__remove btn btn-link" data-product_id="{{ product.id }}" title="Xóa sản phẩm này">&times;</button> </td>
-				</tr>
+		
+		<div class="d-flex">
+			<div class="product-list col-md-7">
 				<#
-			} );
-
-			#>
-			</tbody>
-		</table>
-		<div class="col-12" style="display: flex; justify-content: space-between; padding: 0;">
-			<div class="total-pay-product text-right">
-				<#
-				if( data.voucher ) {
-					if( data.voucher.voucher_type == 'by_price' ) {
-						giam_gia = data.voucher.voucher_price;
-					} else {
-						giam_gia = data.voucher.voucher_price * total / 100;
-					}
+				data.products.forEach( product => {
+					var subtotal = product.price * product.quantity;
+					total += subtotal;
+					total_quantity += parseFloat( product.quantity );
+					id += 1;
 					#>
-					<p><?php esc_html_e( 'Tạm tính:', 'gtt-shop' ) ?> <span class="total__number">{{ eFormatNumber(0, 3, '.', ',', parseFloat( total )) }} <?= $symbol; ?></span></p>
-					<p><?php esc_html_e( 'Giảm giá:', 'gtt-shop' ) ?> <span class="total__number">{{ eFormatNumber(0, 3, '.', ',', parseFloat( giam_gia )) }} <?= $symbol; ?></span><a href="" class="remove-voucher" data-coupon="xtybwpq5">[Xóa]</a></p>
+					<article>
+						<div class="product-item">
+							<a class="post-thumbnail" href="{{ product.link }}" aria-hidden="true" tabindex="-1">
+								<img src="{{product.url}}">
+							</a>
+
+							<div class="product-title">
+								<h3 class="entry-title"><a href="{{ product.link }}">{{ product.title }}</a></h3>
+							</div>
+							<div class="product-price">
+								<p class="price">{{ eFormatNumber(0, 3, '.', ',', parseFloat( product.price )) }} <?= $symbol; ?></p>
+							</div>
+							<!-- <?php ELUSHOP\Cart::add_cart(); ?> -->
+							<div class="cart__quantity"><input type="number" value="{{ product.quantity }}" min="1" data-product_id="{{ product.id }}" style="width: 70px;float: initial;margin: auto;text-align: center;"></div>
+							<div class="cart__remove-product"> <button class="cart__remove btn btn-link" data-product_id="{{ product.id }}" title="Xóa sản phẩm này">&times;</button> </div>
+						</div>
+					</article>
 					<#
-				}
-				cart_subtotal = total - giam_gia;
+				} );
 				#>
-				<p><?php esc_html_e( 'Tổng:', 'gtt-shop' ) ?> <span class="total__number">{{ eFormatNumber(0, 3, '.', ',', parseFloat( cart_subtotal )) }} <?= $symbol; ?></span></p>
+				<div class="product-back">
+					<p>Để thêm sản phẩm vào giỏ hàng, vui lòng quay về trang<a href="<?php echo home_url(); ?>/dat-hang-nhanh"> Đặt hàng nhanh </a></p>
+				</div>
 			</div>
-			<div class="vouchers">
-				<input type="text" name="voucher_code" class="voucher_input" value placeholder="Mã ưu đãi">
-				<button type="submit" class="btn voucher_button" name="apply_voucher" value="Áp dụng"><?php esc_html_e( 'Áp dụng', 'gtt-shop' ); ?></button>
-				<div class="vouchers_message"></div>
+			<div class="product-cart col-md-5">
+				<div class="product-cart__wrapper">
+					<div class="product-cart__detail d-flex">
+						<div class="col-md-5">
+							<p>Số lượng</p>
+							<p class="color-secondary">{{ total_quantity }}</p>
+						</div>
+						<div class="col-md-7">
+							<p>Thành tiền</p>
+							<div class="total-pay-product text-right">
+								<#
+								if( data.voucher ) {
+									if( data.voucher.voucher_type == 'by_price' ) {
+										giam_gia = data.voucher.voucher_price;
+									} else {
+										giam_gia = data.voucher.voucher_price * total / 100;
+									}
+									#>
+									<p><?php esc_html_e( 'Tạm tính:', 'gtt-shop' ) ?> <span class="total__number">{{ eFormatNumber(0, 3, '.', ',', parseFloat( total )) }} <?= $symbol; ?></span></p>
+									<p><?php esc_html_e( 'Giảm giá:', 'gtt-shop' ) ?> <span class="total__number">{{ eFormatNumber(0, 3, '.', ',', parseFloat( giam_gia )) }} <?= $symbol; ?></span><a href="" class="remove-voucher" data-coupon="xtybwpq5">[Xóa]</a></p>
+									<#
+								}
+								cart_subtotal = total - giam_gia;
+								#>
+								<p><?php esc_html_e( 'Tổng:', 'gtt-shop' ) ?> <span class="total__number">{{ eFormatNumber(0, 3, '.', ',', parseFloat( cart_subtotal )) }} <?= $symbol; ?></span></p>
+							</div>
+						</div>
+					</div>
+					<div class="vouchers">
+						<input type="text" name="voucher_code" class="voucher_input" value placeholder="Mã ưu đãi">
+						<button type="submit" class="btn voucher_button" name="apply_voucher" value="Áp dụng"><?php esc_html_e( 'Áp dụng', 'gtt-shop' ); ?></button>
+						<div class="vouchers_message"></div>
+					</div>
+					<div class="product-cart__button">
+						<button class="btn-secondary wp-block-button__link place-order">
+							Tiếp tục thanh toán
+						</button>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="submit-cart-shop text-right">
-			<button class="place-order btn btn-success"><?= __( 'Tiến hành thanh toán', 'gtt-shop' );?></button>
 		</div>
 		<#
 	}
