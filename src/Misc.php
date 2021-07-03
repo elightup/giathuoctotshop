e02991a3b931db89e381a861ac3d1932424d6f12
Anh Tran <anhtnt@elightup.com>
2021-07-03 16:58:12 +0700

    Bỏ admin bar

--- src/Misc.php
+++ src/Misc.php
@@ -4,6 +4,7 @@ namespace ELUSHOP;
 class Misc {
 	public function __construct() {
 		add_filter( 'slim_seo_skipped_shortcodes', [ $this, 'skip_shortcodes' ] );
+		add_action( 'after_setup_theme', [ $this, 'remove_admin_bar' ] );
 	}
 
 	public function skip_shortcodes( $shortcodes ) {
@@ -11,5 +12,11 @@ class Misc {
 		$shortcodes[] = 'mb_user_profile_login';
 		$shortcodes[] = 'mb_user_profile_info';
 		return $shortcodes;
+	}
+
+	public function remove_admin_bar() {
+		if ( !current_user_can( 'edit_posts' ) ) {
+			show_admin_bar( false );
+		}
 	}
 }
