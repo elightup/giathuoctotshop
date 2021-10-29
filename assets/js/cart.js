b46f3881bbe9551667b7902a25023fffcf5bd2d6
longvt93 <vutuanlongdhcn@gmail.com>
2021-10-29 10:45:14 +0700

    Confirm delete product

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -36,6 +36,17 @@
 
 			// Update quantity when input change.
 			$d.on( 'change', '.quantity_products', cart.onChangeQuantity );
+
+			// Click confirm delete
+			$d.on( 'click', '.confirm-delete .btn-primary', function() {
+				let productID = $( this ).data( 'id' );
+				cart.removeProduct( productID );
+				$( '.confirm-delete' ).css( 'display', 'none' );
+				$( '.product-item' ).find( `[data-product='${productID}']` ).find( '.quantity_products' ).val( 0 );
+			} );
+			$d.on( 'click', '.confirm-delete .btn-secondary', function() {
+				$( '.confirm-delete' ).css( 'display', 'none' );
+			} );
 		},
 
 		clear() {
@@ -168,10 +179,18 @@
 			if ( quantity < 0 ) {
 				quantity = 0;
 			}
-
 			$quantityInput.val( quantity );
 
 			const productId = $parent.data( 'product' );
+
+			// Confirm delete product
+			if ( cart.hasProduct( productId ) && quantity == 0 ) {
+				cart.showConfirmDelete( productId );
+				$quantityInput.val( quantity + 1 );
+				return;
+			}
+
+			// Update product bt
 			if ( cart.hasProduct( productId ) ) {
 				cart.updateProduct( productId, quantity );
 			} else {
@@ -198,10 +217,9 @@
 			}
 		},
 		showToast() {
-
 			const toast =
 			 `<div class="toast">
-				<p class="title">Sản phảm đã thêm vào Giỏ hàng</p>
+				<p class="title">Sản phẩm đã thêm vào Giỏ hàng</p>
 				<div class="img-toast">
 					<svg class="svg-icon" viewBox="0 0 20 20">
 						<path d="M10.219,1.688c-4.471,0-8.094,3.623-8.094,8.094s3.623,8.094,8.094,8.094s8.094-3.623,8.094-8.094S14.689,1.688,10.219,1.688 M10.219,17.022c-3.994,0-7.242-3.247-7.242-7.241c0-3.994,3.248-7.242,7.242-7.242c3.994,0,7.241,3.248,7.241,7.242C17.46,13.775,14.213,17.022,10.219,17.022 M15.099,7.03c-0.167-0.167-0.438-0.167-0.604,0.002L9.062,12.48l-2.269-2.277c-0.166-0.167-0.437-0.167-0.603,0c-0.166,0.166-0.168,0.437-0.002,0.603l2.573,2.578c0.079,0.08,0.188,0.125,0.3,0.125s0.222-0.045,0.303-0.125l5.736-5.751C15.268,7.466,15.265,7.196,15.099,7.03"></path>
@@ -212,8 +230,17 @@
 			$( 'body' ).append( toast ).fadeTo( 2000, 1, function() {
 				$( '.toast' ).css( 'z-index', '0' ).fadeTo( 1000, 0 );
 			} );
+		},
+		showConfirmDelete( productId ) {
+			const formDelete = `<div id="confirm-delete" class="confirm-delete">
+				<h3>Xin xác nhận</h3>
+				<p>Bạn có chắc chắn muốn xóa sản phẩm này khỏi giỏ hàng.</p>
+				<a class="btn-primary wp-block-button__link" data-id="${productId}">Có</a>
+				<a class="btn-secondary wp-block-button__link popup-modal-dismiss">Không</a>
+			</div>`;
 
-		}
+			$( 'body' ).append( formDelete );
+		},
 	};
 
 	cart.init();
