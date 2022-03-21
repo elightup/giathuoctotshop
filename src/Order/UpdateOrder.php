f03715532eb24bc1f4c9564804363916df3cc8f5
longvt93 <vutuanlongdhcn@gmail.com>
2022-03-21 11:30:17 +0700

    Làm notifications

--- src/Order/UpdateOrder.php
+++ src/Order/UpdateOrder.php
@@ -28,6 +28,18 @@ class UpdateOrder {
 			[ 'data_update' => json_encode( $products_not_delivered ) ],
 			[ 'id' => $id ]
 		);
+
+		$user_id = $wpdb->get_row( $wpdb->prepare( "SELECT `user` FROM {$wpdb->prefix}orders WHERE `id`=%d", $id ) );
+		$wpdb->insert(
+			$wpdb->notifications,
+			[
+				'user'     => $user_id->user,
+				'order_id' => $id,
+				'date'     => current_time( 'mysql' ),
+				'status'   => 'unread',
+				'note'     => '',
+			]
+		);
 		$output['message'] = 'Đã update order thành công';
 
 		return $output;
